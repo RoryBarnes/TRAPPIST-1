9baf25b7ec75bf198b6410d5f86753f8a621223e
Rory Barnes <rory@kali.astro.washington.edu>
2020-01-10 11:48:56 -0800

    Adding input files and python scripts to generate the tidal heating plot.

---
+++ TideHeat/makeplot.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+import numpy as np
+import subprocess as subp
+import string as str
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+file=open('trappist1.tideheat.out')
+nlines=996
+
+e=[0 for j in range(nlines)]
+hb=[0 for j in range(nlines)]
+hc=[0 for j in range(nlines)]
+hd=[0 for j in range(nlines)]
+he=[0 for j in range(nlines)]
+hf=[0 for j in range(nlines)]
+hg=[0 for j in range(nlines)]
+hh=[0 for j in range(nlines)]
+
+j=0
+for line in file:
+    words=str.split(line)
+    e[j]=float(words[0])
+    hb[j]=float(words[1])
+    hc[j]=float(words[2])
+    hd[j]=float(words[3])
+    he[j]=float(words[4])
+    hf[j]=float(words[5])
+    hg[j]=float(words[6])
+    hh[j]=float(words[7])
+    j += 1
+
+fbk = {'lw':0.0, 'edgecolor':None}
+fig = plt.figure(figsize=(10,8), dpi=300,facecolor='black')
+
+fig.subplots_adjust(wspace=.05,left=.01,bottom=.01)
+ax = fig.add_subplot(1,1,1,axisbg='k')
+
+ax.spines['top'].set_color('white')
+ax.spines['top'].set_linewidth(2)
+ax.spines['right'].set_color('white')
+ax.spines['right'].set_linewidth(2)
+ax.spines['bottom'].set_linewidth(2)
+ax.spines['bottom'].set_color('white')
+ax.spines['left'].set_color('white')
+ax.spines['left'].set_linewidth(2)
+
+ax.title.set_color('white')
+ax.yaxis.label.set_color('white')
+ax.xaxis.label.set_color('white')
+ax.tick_params(axis='x', which ='both',colors='white')
+ax.tick_params(axis='y', which = 'both',colors='white')
+    
+plt.rcParams['axes.linewidth'] = 3
+plt.xlim(0.001,0.1)
+plt.ylim(1e-3,1000)
+#plt.xscale('log')
+plt.yscale('log')
+plt.tick_params(axis='both', labelsize=20,width=3,length=10,top='on',right='on')
+plt.xlabel('Eccentricity',fontsize=25)
+plt.ylabel('Surface Energy Flux (W/m$^2$)',fontsize=25)
+
+#plt.fill_betweenx([1e-5,0.2],[1e-5,0.2],[1e-5,0.08],color='0.85', **fbk)
+ax.add_patch(patches.Rectangle((1e-5,1e-5),0.2,0.08,color='#2C6F4C'))
+ax.add_patch(patches.Rectangle((1e-5,0.08),0.2,2.92,color='#cc9900'))
+ax.add_patch(patches.Rectangle((1e-5,3),0.2,297,color='#ff9933'))
+ax.add_patch(patches.Rectangle((1e-5,300),0.2,700,color='#cc0000'))
+
+
+plt.plot(e,hb,color='w',linewidth=3)
+plt.text(0.012,350,'b',color='w',fontsize=25)
+
+plt.plot(e,hc,color='w',linewidth=3)
+plt.text(0.013,70,'c',color='w',fontsize=25)
+
+plt.plot(e,hd,color='w',linewidth=3)
+plt.text(0.015,3,'d',color='w',fontsize=25)
+
+plt.plot(e,he,color='w',linewidth=3)
+plt.text(0.016,0.7,'e',color='w',fontsize=25)
+
+plt.plot(e,hf,color='w',linewidth=3)
+plt.text(0.018,0.15,'f',color='w',fontsize=25)
+
+plt.plot(e,hg,color='w',linewidth=3)
+plt.text(0.021,0.05,'g',color='w',fontsize=25)
+
+plt.plot(e,hh,color='w',linewidth=3)
+plt.text(0.035,0.004,'h',color='w',fontsize=25)
+
+
+plt.savefig('trappist1.tideheat.png',bbox_inches='tight',facecolor=fig.get_facecolor())
+plt.close()
+
+
---
+++ TideHeat/survey.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+import numpy as np
+import subprocess as subp
+import string
+#import matplotlib.pyplot as plt
+
+def GetLogValue(file,body,param):
+    logfile=open(file)
+    current=0
+    for line in logfile:
+        words=str.split(line)
+        if len(words) > 1:
+            if 'BODY' in words[1]:
+                if body == words[2]:
+                    current=1
+            if current == 1 and param in words[0]:
+                last=len(words)-1
+                return words[last]
+            
+
+emin=1e-5
+emax=0.2
+de=1.01
+npl=7
+
+e=emin
+necc=0
+while e <= emax:
+    necc += 1
+    e *= de
+
+#print(necc)
+
+heat=[[0 for j in range(necc)] for k in range(npl)]
+ecc=[0 for j in range(necc)]
+
+name = ['b','c','d','e','f','g','h']
+infile =['b.tmp','c.tmp','d.tmp','e.tmp','f.tmp','g.tmp','h.tmp']
+outfile =['b.in','c.in','d.in','e.in','f.in','g.in','h.in']
+heatfile=open('trappist1.tideheat.out','w')
+
+e=emin
+n=0
+while e <= emax:
+    ecc[n] = e
+    for j in range(npl):
+        cmd1="cp "+infile[j]+" "+outfile[j]
+        subp.call(cmd1,shell=True)
+        cmd2="echo dEcc    "+repr(e)+" >> "+outfile[j]
+        subp.call(cmd2,shell=True)
+    #exit()
+    cmd3="/Users/rory/bin/vplanet vpl.in >& dump"
+    subp.call(cmd3,shell=True)
+
+    # Now get heat fluxes
+    print(n)
+    heatfile.write(repr(e)+' ')
+    for j in range(npl):
+        heat[j][n] = float(GetLogValue('trappist1.log',name[j],'SurfEnFluxEqtide'))
+        heatfile.write(repr(heat[j][n])+' ')
+        #print(heat[j][0])
+    heatfile.write('\n')
+    n += 1
+    e *= de
+    #exit()
+
+
+
+#plt.plot(ecc,heat[0])
+#plt.savefig('trappist1.tidalheat.png')
