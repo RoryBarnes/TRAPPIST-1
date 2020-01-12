8375dfad76e500dc9cc1afb3d0bc014754370b32
Rory Barnes <rory@kali.astro.washington.edu>
2020-01-12 15:01:28 -0800

    Made slightly squatter.
    Added planetary semi-major axes to the HZ panel.

--- AtmEsc/makeplot.py
+++ AtmEsc/makeplot.py
@@ -18,7 +18,7 @@ if (sys.argv[1] != 'pdf' and sys.argv[1] != 'png'):
     exit(1)
 
 #Typical plot parameters that make for pretty plot
-mpl.rcParams['figure.figsize'] = (6.5,9)
+mpl.rcParams['figure.figsize'] = (6.5,6.5)
 mpl.rcParams['font.size'] = 12.0
 
 # Load data
@@ -57,23 +57,33 @@ axes[1,0].plot(time, output.star.Temperature,'k')
 
 # Format
 axes[1,0].set_xlim(1e6,time.max())
-axes[1,0].set_ylim(0.9*output.star.Temperature.min(),1.1*output.star.Temperature.max())
+axes[1,0].set_ylim(0.95*output.star.Temperature.min(),1.05*output.star.Temperature.max())
 axes[1,0].set_xlabel("Time [yr]")
 axes[1,0].set_ylabel(r"T$_{eff}$ (K)")
 axes[1,0].set_xscale('log')
 
 ## Middle Right: Habitable Zone ##
-axes[1,1].plot(time, output.star.HZLimRecVenus,'k',linestyle='dashed')
-axes[1,1].plot(time, output.star.HZLimRunaway,'k')
-axes[1,1].plot(time, output.star.HZLimMaxGreenhouse,'k')
-axes[1,1].plot(time, output.star.HZLimEarlyMars,'k',linestyle='dashed')
+axes[1,1].plot(time, output.star.HZLimRecVenus,'g',linestyle='dashed')
+axes[1,1].plot(time, output.star.HZLimRunaway,'g')
+axes[1,1].plot(time, output.star.HZLimMaxGreenhouse,'g')
+axes[1,1].plot(time, output.star.HZLimEarlyMars,'g',linestyle='dashed')
 
 # Format
 axes[1,1].set_xlim(1e6,time.max())
-axes[1,1].set_ylim(0,0.5)
+axes[1,1].set_ylim(0.005,0.1)
 axes[1,1].set_xlabel("Time [yr]")
-axes[1,1].set_ylabel("Habitable Zone (AU)")
+axes[1,1].set_ylabel("Hab. Zone (AU)")
 axes[1,1].set_xscale('log')
+
+# Add planets
+axes[1,1].plot([0,5e9],[0.01154775,0.01154775],'k')
+axes[1,1].plot([0,5e9],[0.01581512,0.01581512],color=vpl.colors.red)
+axes[1,1].plot([0,5e9],[0.02228038,0.02228038],color=vpl.colors.orange)
+axes[1,1].plot([0,5e9],[0.02928285,0.02928285],color=vpl.colors.pale_blue)
+axes[1,1].plot([0,5e9],[0.03853361,0.03853361],color=vpl.colors.purple)
+axes[1,1].plot([0,5e9],[0.04687692,0.04687692],color=vpl.colors.dark_blue)
+axes[1,1].plot([0,5e9],[0.06193488,0.06193488],color='grey')
+axes[1,1].set_yticks([0.01,0.05,0.1])
 
 ## Bottom Left: Surface water ##
 axes[2,0].plot(time, output.b.SurfWaterMass,'k')
@@ -95,7 +105,7 @@ axes[2,1].plot(time, output.c.OxygenMass,color=vpl.colors.red,label='c')
 axes[2,1].plot(time, output.d.OxygenMass,color=vpl.colors.orange,label='d')
 axes[2,1].plot(time, output.e.OxygenMass,color=vpl.colors.pale_blue,label='e')
 axes[2,1].plot(time, output.f.OxygenMass,color=vpl.colors.purple,label='f')
-axes[2,1].legend(loc='best')
+axes[2,1].legend(loc='best',prop={'size': 11})
 
 # Format
 axes[2,1].set_xlim(1e6,time.max())
